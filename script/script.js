0ac58a7f35dabbdf04fb77045698c3422bc909b0
BlackCloak <ars081190@yandex.ru>
2021-06-24 23:50:29 +0300

    исправить ошибки

--- script/script.js
+++ script/script.js
@@ -1,26 +1,25 @@
-console.log('hi');
 const page = document.querySelector('.main')
 const popupEdit = page.querySelector('.popup')
 const popupCloseButton = page.querySelector('.popup__close-button')
 const popupOpenButton = page.querySelector('.profile__edit')
-
-console.log({popupEdit, popupCloseButton, popupOpenButton})
-
-const popupVisible = function() {
-    popupEdit.classList.add('popup_open')
-}
-
-const popupInvisible = function() {
-    popupEdit.classList.remove('popup_open')
-}
-popupOpenButton.addEventListener('click', popupVisible)
-popupCloseButton.addEventListener('click', popupInvisible)
-
 const profileNameDef = document.querySelector('.profile__name')
 const profileJobDef = document.querySelector('.profile__description')
 const profileForm = document.querySelector('.popup__content')
 const profileName = document.querySelector('.popup__name')
 const profileJob = document.querySelector('.popup__description')
+
+const popupVisible = function() {
+    popupEdit.classList.add('popup_open')
+    profileName.value = profileNameDef.textContent
+    profileJob.value = profileJobDef.textContent
+}
+
+const popupInvisible = function() {
+    popupEdit.classList.remove('popup_open')
+}
+
+
+
 /*const submit = document.querySelector('.popup__submit')*/
 
 const profileEdit = function(evt) {
@@ -31,5 +30,6 @@ const profileEdit = function(evt) {
 }
 
 
-
+popupOpenButton.addEventListener('click', popupVisible)
+popupCloseButton.addEventListener('click', popupInvisible)
 profileForm.addEventListener('submit', profileEdit)
